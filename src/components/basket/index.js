adeb9c506c72e76800e31ceef076eb13fdf28925
Murathan Akin <murathan.akin@hotmail.com>
2022-06-06 17:53:01 +0300

    use localStorage to store added products to the cart

--- src/components/basket/index.js
+++ src/components/basket/index.js
@@ -26,6 +26,7 @@ const Basket = () => {
 			tempTotalPrice += product.price * product.count;
 		});
 		setTotalPrice(tempTotalPrice);
+		localStorage.setItem('basketItems', JSON.stringify(addedProducts));
 	}, [addedProducts]);
 	return (
 		<>
